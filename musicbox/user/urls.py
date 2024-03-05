b5123847734d11e698bbdcf7884b9d0f4cff3a37
Usmaan Wahab <usmaanwahab11@outlook.com>
2024-03-05 23:30:15 +0000

    removed user code so that Harvey is able to work on his tasks

--- musicbox/user/urls.py
+++ musicbox/user/urls.py
@@ -3,8 +3,4 @@ from django.urls import path, re_path
 
 urlpatterns = [
     path('', index, name='index'),
-    path('login/', login, name='login'),
-    path('register/', register, name='register'),
-    path('manage_account/', manage_account, name='manage account')
-
 ]
