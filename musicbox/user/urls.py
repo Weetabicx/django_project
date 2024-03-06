d8080a77790071a09f07c1c972aaf7b1c4b084bc
Usmaan Wahab <usmaanwahab11@outlook.com>
2024-03-06 20:15:37 +0000

    user prep

--- musicbox/user/urls.py
+++ musicbox/user/urls.py
@@ -3,4 +3,7 @@ from django.urls import path, re_path
 
 urlpatterns = [
     path('', index, name='index'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+
 ]
--- musicbox/user/views.py
+++ musicbox/user/views.py
@@ -4,6 +4,14 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
-
+# Create your views here.
 def index(request):
     return render(request, 'user/index.html')
+
+
+def login(request):
+    return render(request, 'user/login.html')
+
+
+def register(request):
+    return render(request, 'user/register.html')
