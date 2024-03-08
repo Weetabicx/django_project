fcccacb5850fffaaa846377ac74dc8f388c00c00
Usmaan Wahab <usmaanwahab11@outlook.com>
2024-03-08 02:04:26 +0000

    Models created by Harvey have been uploaded

--- musicbox/album/models.py
+++ musicbox/album/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Album(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    type = models.CharField(max_length=30) #E.g:Album,Single,EP
+    slug = models.SlugField(unique=True)
+    genre = models.CharField(max_length=30)
+    release_date = models.DateField()
+    artist = models.CharField(max_length=50)
+    owner = models.ForeignKey(User)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.type + self.name)
+        super(Album, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+class Album_Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.CharField(300)
+    
+
+    def __str__(self):
+        return self.title
--- musicbox/song/models.py
+++ musicbox/song/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from musicbox.album.models import Album
 
 # Create your models here.
+class Song(models.Model):
+    id = models.AutoField(primary_key=True)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    genre = models.CharField(max_length=30)
+    release_date=models.DateField() #date/artist are automatically set ot the album's date/artist
+    artist = models.CharField(max_length=50)
+    
+
+    def __str__(self):
+        return self.title
+
+class Song_Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    album = models.ForeignKey(Song, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.CharField(300)
+    
+    def __str__(self):
+        return self.title    
