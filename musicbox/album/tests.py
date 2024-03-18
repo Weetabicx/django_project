bcc44d38d0e5f202735dff4a7454374b0ab4d810
Usmaan Wahab <usmaanwahab11@outlook.com>
2024-03-18 04:42:54 +0000

    created basic album test

--- musicbox/album/tests.py
+++ musicbox/album/tests.py
@@ -1,3 +1,25 @@
+import importlib
 from django.test import TestCase
 
 # Create your tests here.
+HEADER = "\n\n==============================!"
+FOOTER = "!==============================\n"
+class AlbumModelsTestCase(TestCase):
+    """Tests for the album models, this test assume the populaton script has been runs."""
+
+    def setUp(self,):
+        pass
+
+
+class AlbumViewsTestCase(TestCase):
+    """Tests for the album models and views, this test assume the populaton script has been runs."""
+
+    def setUp(self,):
+        self.views_module = importlib.import_module('album.views')
+        self.views_module_listing = dir(self.views_module)
+
+    def test_views_exist(self,):
+        """Test that the correct views exists."""
+        views = ['albums_list', 'upload_album', 'album_update', 'album_delete', 'album_detail']
+        for view in views:
+            self.assertIn(view, self.views_module_listing, f"{HEADER}View {view} not found in views module.{FOOTER}")
