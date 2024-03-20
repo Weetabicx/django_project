e89b579a391dda7a462f7dba030b1d707ee81612
Usmaan Wahab <usmaanwahab11@outlook.com>
2024-03-20 16:41:44 +0000

    fixed urls

--- musicbox/album/urls.py
+++ musicbox/album/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from . import views
 from .views import *
 from django.conf.urls.static import static
 from django.conf import settings
@@ -9,10 +8,10 @@ app_name = 'album'
 urlpatterns = [
                 path('', albums_list, name='list'),
                 path('upload/', upload_album, name='upload'),
-                path('<int:id>/update/', album_update, name='update'),
-                path('<int:id>/delete/', album_delete, name='delete'),
+                path('<int:id>/edit/', edit_album, name='edit'),
+                path('<int:id>/delete/', delete_album, name='delete'),
                 path('<int:album_id>/', album_detail, name='detail'),
-                path('search/<str:query>/', search, name='search'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
